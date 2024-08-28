b1cbb00918072a12c0cc96e56057d19b743e206a
Wellsaik <artem.podorozhko@outlook.com>
2024-08-28 19:58:11 +0300

    v1.0.1

--- class_u_logger_settings.js
+++ class_u_logger_settings.js
@@ -1,6 +1,8 @@
 var class_u_logger_settings =
 [
     [ "ULoggerSettings", "class_u_logger_settings.html#a4f48e821625da31478ef38d5e616f8f8", null ],
+    [ "UPROPERTY", "class_u_logger_settings.html#a862ce95fbde90e525e5d6e74e061bdf7", null ],
+    [ "UPROPERTY", "class_u_logger_settings.html#a3b836a7f3061d8eddf6f0c0c857d5f77", null ],
     [ "UPROPERTY", "class_u_logger_settings.html#a7d58dc26228c2d7e08113db675e4b9f7", null ],
     [ "UPROPERTY", "class_u_logger_settings.html#acdafb2d525f30042325bc71791ecc617", null ],
     [ "UPROPERTY", "class_u_logger_settings.html#ace00bb7af96a8c6ff2845edef8392a3b", null ]
